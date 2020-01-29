6b1eb441fedcab230b611324801a6a6c081dfed0
Voltzy9 <arthur.genthial@ynov.com>
2020-01-29 17:03:08 +0100

    add slidedtimed

--- React/components/Header.js
+++ React/components/Header.js
@@ -5,9 +5,9 @@ import { View, Button, StyleSheet, Text, Image } from 'react-native';
 class Header extends React.Component {
     render() {
         return(
-            <View>
-                <Text style={styles.Header}>ARTG</Text>
-                <Image source={require('../assets/icon/icons8-menu-24.png')} alt="Logo" />
+            <View style={styles.Header}>
+                <Text style={styles.Text}>ARTG</Text>
+                <Image style={styles.logo} source={require('../assets/icon/icons8-menu-24.png')} alt="Logo" />
             </View>
         )
     }
@@ -17,12 +17,13 @@ export default Header
 const styles = StyleSheet.create({
     Header: {
         marginTop:50,
-        justifyContent: 'center',
         alignItems: 'center',
     },
-    // logo: {
-    //     marginTop:50,
-    //     justifyContent: 'right',
-    //     alignItems: 'right',
-    // }
+    Text: {
+        flex:0,
+        justifyContent: 'center',
+    },
+    logo: {
+        flex:0,
+    }
 });
--- React/components/Homepage.js
+++ React/components/Homepage.js
@@ -1,21 +1,52 @@
 import React from "react";
-import { View, TextInput, Button, StyleSheet, Text, ImageBackground, Image } from 'react-native';
 import Header from "./Header";
+import TimedSlideshow from 'react-native-timed-slideshow';
+import { View, TextInput, Button, StyleSheet, Text, ImageBackground, Image } from 'react-native';
 
 
 class Homepage extends React.Component {
     render() {
+        const items = [
+            {
+                uri: "http://www.lovethemountains.co.uk/wp-content/uploads/2017/05/New-Outdoor-Sports-and-Music-Festival-For-Wales-4.jpg",
+                title: "New",
+                text: "Minnesota, USA",
+                duration: 6000,
+                fullWidth: true
+            },
+            {
+                uri: "http://blog.adrenaline-hunter.com/wp-content/uploads/2018/05/bungee-jumping-barcelona-1680x980.jpg",
+                title: "Victor Fallon",
+                text: "Val di Sole, Italy",
+                buton: "Discover",
+                direction:'odd',
+                duration: 6000,
+                fullWidth: true
+            },
+            {
+                uri: "https://greatist.com/sites/default/files/Running_Mountain.jpg",
+                title: "Mary Gomes",
+                text: "Alps",
+                duration: 6000,
+                fullWidth: true
+            }
+        ];
         return(
-            <View>
-                {/*<ImageBackground source={require('../assets/voitures/Bentley-Continental-GT-luxury-black-car_iphone_640x1136.jpg')}/>*/}
-               <Header/>
-                {/*<View style={styles.main_content}>*/}
-                {/*    <Text style={styles.Text}>New</Text>*/}
-                {/*    <Text  style={styles.Text} >Continental GTC</Text>*/}
-                {/*    <Button title='Discover' onPress={() => {}}></Button>*/}
-                {/*</View>*/}
-            </View>
-        );
+            <View style={styles.body}>
+                <TimedSlideshow
+                    items={items}
+                />
+                <Header/>
+                <View style={styles.main_content}>
+                    <Text style={styles.Text}>New</Text>
+                    <Text  style={styles.Text} >Continental GTC</Text>
+                    <Button title='Discover' onPress={() => {}}></Button>
+
+                </View>
+
+               </View>
+
+        )
     }
 }
 
@@ -23,10 +54,14 @@ export default Homepage
 
 const styles = StyleSheet.create({
     body: {
+        flex: 1
+    },
+    Slide: {
+      position:'absolute',
+        left: 0,
+        top:0
     },
     main_content: {
-        marginTop:30,
-        flex:0,
         justifyContent:'center',
         alignItems: 'center',
     },
