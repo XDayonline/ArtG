8b36d6237760ffa7cc9a51d6a11fe3265ebdccb5
Genthial Athur <voltzy@pop-os.localdomain>
2020-03-10 17:58:05 +0100

    catalog + props case

--- React/components/Catalog.js
+++ React/components/Catalog.js
@@ -1,42 +1,54 @@
 import React from "react";
-import {ImageBackground, StyleSheet, Text, View} from "react-native";
+import {default as styled, StyleSheet, Text, View, Image, ScrollView} from "react-native";
 import Header from "./Header";
-import {Button} from "react-native-elements";
+import {Button} from 'react-native-elements';
+
+
+const Case = styled.view`
+    // backgroundColor: ${props => props.backColor};
+    
+    justifyContent:'center';
+    alignItems: 'center';
+    zIndex:1;
+    `;
 
 class Catalog extends React.Component {
     render() {
         return(
             <View style={styles.body}>
                 <Header navigation={this.props.navigation}/>
-                <View>
-                        <View style={styles.main_content}>
-                            <Text style={styles.Text}>New</Text>
-                            <Text  style={styles.Text} >Continental GTC</Text>
-                            <Button buttonStyle={{borderColor:'white', borderWidth:2}} type='outline' titleStyle={{color:'white'}} style={styles.Btn} title='Discover' />
-                        </View>
-                </View>
-                <View>
+                <ScrollView>
                     <View style={styles.main_content}>
                         <Text style={styles.Text}>New</Text>
-                        <Text  style={styles.Text} >Continental GTC</Text>
                         <Button buttonStyle={{borderColor:'white', borderWidth:2}} type='outline' titleStyle={{color:'white'}} style={styles.Btn} title='Discover' />
+                        <Image source={require('../assets/cars/vantage-mobile-navigation.png')} style={{width:400, height: 400}} />
                     </View>
-                </View>
+                    <Case backColor="red">
+                        <Text style={styles.Text}>New</Text>
+                        <Button buttonStyle={{borderColor:'white', borderWidth:2}} type='outline' titleStyle={{color:'white'}} style={styles.Btn} title='Discover' />
+                        <Image source={require('../assets/cars/db11-mobile-navigation.png')} style={{width:400, height: 400}}/>
+                    </Case>
+                    <Case style={styles.main_content}>
+                        <Text style={styles.Text}>New</Text>
+                        <Button buttonStyle={{borderColor:'white', borderWidth:2}} type='outline' titleStyle={{color:'white'}} style={styles.Btn} title='Discover' />
+                        <Image source={require('../assets/cars/vantage-mobile-navigation.png')} style={{width:400, height: 400}} />
+                    </Case>
+                    <View style={styles.main_content}>
+                        <Text style={styles.Text}>New</Text>
+                        <Button buttonStyle={{borderColor:'white', borderWidth:2}} type='outline' titleStyle={{color:'white'}} style={styles.Btn} title='Discover' />
+                        <Image source={require('../assets/cars/am700_thumb.png')}  style={{width:400, height: 400}}/>
+                    </View>
+                </ScrollView>
             </View>
         )
     }
 }
 export default Catalog;
 
+
 const styles = StyleSheet.create({
     body: {
         flex: 1
-    },
-    backgroundImage: {
-        flex:1,
-        position:'absolute',
-        // zIndex:0,
-        resizeMode:'cover',
     },
     main_content: {
         marginTop:150,
